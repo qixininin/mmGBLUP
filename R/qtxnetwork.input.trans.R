e177764e04cb302f1d8fcc99dab89bd926405668
qixininin <qixin0909@gmail.com>
2024-10-16 08:38:25 +0800

    solve bug2 in qtxnetwork.input.trans()

--- R/qtxnetwork.input.trans.R
+++ R/qtxnetwork.input.trans.R
@@ -58,7 +58,7 @@ qtxnetwork.input.trans <- function(geno_data, pheno_data, geno_output_prefix, ph
     write(paste0("_TotalMarker\t", sum(chr), "\t", paste(chr, collapse = " ")), printer, append = T)
     write("_MarkerCode\tP1=1\tP2=-1\tF1=0\n", printer, append = T)
     write("*TraitBegin*", printer, append = T)
-    write.table(pheno_data[,c(1,c+1)], file = printer, append = T,
+    write.table(pheno_data[,c(1,c+1)], file = printer, append = T, na = ".",
                 row.names = F, col.names = T, quote = F,sep = "\t", eol = ";\n")
     write("*TraitEnd*", printer, append = T)
     close(printer)
